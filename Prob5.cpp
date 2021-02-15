5d281817a211e2ad6029be1f902e6cdb0e554be3
RogerMei <rogermei@umich.edu>
2021-02-15 17:02:52 -0500

    Prob5 solution

--- Prob5.cpp
+++ Prob5.cpp
@@ -1,48 +1,40 @@
- vector<int> distanceK(TreeNode* root, TreeNode* target, int K) 
+ /*
+ Given a string, you need to output the least number of characters you need to change, s.t. any character
+ is different from their neibors
+ */
+
+#include <iostream>
+
+int Solver(std::string s)
 {
-    int trgetLevel; // locate the target 
-    int level = 0; // initialize level
-    queue<Node*> q; // initialize queue for BFS
-    q.push(root); // push the root node into BFS
-    unordered_map<int, vector<int>> Dict; // key-> level, value->numbers in the level
-    vector<int> res; // initialize resolution
-    
-    
-    //BFS
-    while (!q.empty())
+    /*
+    * This is the primary solver function 
+    */ 
+
+    int res = 0; // initialize resolution
+
+    // we need to find out number of pairs
+    for (int idx = 0; idx < s.length(); idx++)
     {
-        int q_size = q.size();
-        
-        while(q_size-->0)
-        {
-            Node* curr_node = q.front(); // retrieve the first element in the queue
-            Dict[level].push_back(root->val); // push the value to the current level
-            q.pop(); // pop the first element
-            
-            if (curr_node->val == target->val)
-            {
-                targetLevel = level; // locate the target node
-            }
-        }
-        
-        level++; // increment level
-    }
-    
-    // procerss the relative distance
-    if (Dict[targetLevel + K].size()) // examine and traverse the lower side
-    {
-        for (auto a : Dict[targetLevel+K])
-        {
-            res.push_back(a);
+        if (s[idx] == s[idx+1])
+        {   
+            res++; 
+            res += Solver(s.substr(idx+2));
+            return res;
         }
     }
+
+    return res;
     
-    if ( (targetLevel - K) >= 0 )//examine and traverse the upper side (don't need to traverse through the root node)
-    {
-        for (auto a : Dict[targetLevel+K])
-        {
-            res.push_back(a);
-        }
-    }
-    
+}
+
+int main() 
+{
+    std::string s = "aaaabsbs";
+
+    int res = Solver(s);
+
+    std::cout << "You need to modify " << res << " numbers" << std::endl;
+
+    return 0;
 }
